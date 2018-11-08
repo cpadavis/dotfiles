40d387d93be5e038f2751d461c97a9119307126e
cpadavis <chris.pa.davis@gmail.com>
2018-11-07 17:44:29 -0800

    oops didnt mean to get rid of niceties

---
+++ setup_scripts/niceties.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# these are things that will make my dev experience nicer, but I don't think are necessary for running anything
+# note that they overlap with the macbook setup.
+
+# clone dotfiles
+cd ${HOME}
+git clone https://github.com/cpadavis/dotfiles.git .dotfiles
+
+# do linking
+cd ${HOME}/.dotfiles
+# init the vim submodules
+git submodule update --init
+
+mkdir ${HOME}/ipynbs
+mkdir ${HOME}/.jupyter
+
+# vim
+ln -s ${HOME}/.dotfiles/vimrc ${HOME}/.vimrc
+ln -s ${HOME}/.dotfiles/vim ${HOME}/.vim
+# tmux
+ln -s ${HOME}/.dotfiles/tmux.conf ${HOME}/.tmux.conf
+ln -s ${HOME}/.dotfiles/tmux ${HOME}/.tmux
+# ipython
+ln -s ${HOME}/.dotfiles/ipython ${HOME}/.ipython
+ln -s ${HOME}/.dotfiles/ipython/profile_nbserver/ipython_notebook_config.py ${HOME}/.jupyter/jupyter_notebook_config.py
+# git
+ln -s ${HOME}/.dotfiles/gitconfig ${HOME}/.gitconfig
+# directory colors
+ln -s ${HOME}/.dotfiles/dircolors-solarized/dircolors.ansi-universal ${HOME}/.dircolors
+# tmuxline default. promptline is dealt with in zshenv
+ln -s ${HOME}/.dotfiles/tmuxline/tmuxline_LuciusLight.conf ${HOME}/.dotfiles/tmuxline/tmuxline.conf
+
+# iterm2 with my modifications
+ln -s ${HOME}/.dotfiles/iterm2/iterm2_shell_integration.bash ${HOME}/.iterm2_shell_integration.bash
+ln -s ${HOME}/.dotfiles/iterm2/iterm2_shell_integration.zsh ${HOME}/.iterm2_shell_integration.zsh
+ln -s ${HOME}/.dotfiles/iterm2 ${HOME}/.iterm2
+
+# bash profile
+mv ${HOME}/.profile ${HOME}/.profile.bak
+ln -s ${HOME}/.dotfiles/zshenvs/profile_gcloud ${HOME}/.profile
+
+# link also zsh
+ln -s ${HOME}/.dotfiles/zshrc ${HOME}/.zshrc
+ln -s ${HOME}/.dotfiles/zshenvs/zshenv_GCLOUD ${HOME}/.zshenv
+
+
+# enable widgets with jupyter
+jupyter nbextension enable --py widgetsnbextension
+
+
+echo "change shell with chsh"
--- setup_scripts/python.sh
+++ setup_scripts/python.sh
@@ -10,17 +10,14 @@ pip3 install "descarteslabs[complete]"
 # not actually sure this will work if cloudpickle got itself installed earlier...
 pip3 install cloudpickle==0.4.0
 
-# install Keras
-pip3 install keras==2.2.2
-
-# Install Tensorflow (v1.10)
-pip3 install tensorflow-gpu==1.10
-
+# install Keras (v2.2.2) and tensorflow (v1.10)
+pip3 install keras==2.2.2 tensorflow-gpu==1.10
+# note: pip3 install . in appsci_utils overwrites tensorflow-gpu version??
 
 git clone https://github.com/descarteslabs/appsci_utils.git
 
 cd appsci_utils
-pip3 install .
+python setup.py install --user
 echo "Testing flake8"
 flake8
 echo "Running unit tests"
@@ -39,6 +36,20 @@ elif len(sys.argv) > 2:
     raise ValueError('not sure what to do with {0}'.format(str(sys.argv)))
 else:
     gpu_num = 0
+
+print('testing cpu at /cpu:0')
+
+with tf.device('/cpu:0')
+    a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3], name='a')
+    b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2], name='b')
+    c = tf.matmul(a, b)
+
+with tf.Session() as sess:
+    for i in range(200):
+        if i == 0 or i == 199:
+            print (sess.run(c))
+
+print('if you got here, tensorflow at least runs on a cpu')
 
 print('testing gpu at /gpu:{0}'.format(gpu_num))
 
